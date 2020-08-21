9b252b8950cf6087c801b28a68983dc7cba8b7cf
shwjdgh34 <nossi8128@gmail.com>
2020-08-21 14:17:02 +0900

    Solve twosum

---
+++ Problems/twosum.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for i, v in enumerate(nums):
+            another = target - v
+
+            if another in seen:
+                return [seen[another], i]
+            else:
+                seen[v] = i
+
+        return [-1, -1]
+
+
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j];
