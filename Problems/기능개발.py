0e87a0789f708fa8117184cbdbc928b409d4ec5a
shwjdgh34 <nossi8128@gmail.com>
2020-09-07 16:18:18 +0900

    solve 기능개발

--- Problems/기능개발.py
+++ Problems/기능개발.py
@@ -1,3 +1,7 @@
+'''
+Stack
+O(n)
+'''
 import math
 
 
@@ -5,14 +9,53 @@ def solution(progresses, speeds):
 
     n = len(progresses)
     answer = []
-    term = [0] * n
+    terms = []
     for i in range(n):
-        term[i] = math.ceil((100 - progresses[i]) / speeds[i])
+        cur = math.ceil((100 - progresses[i]) / speeds[i])
+        if len(terms) == 0:
+            terms.append(cur)
+        elif terms[0] >= cur:
+            terms.append(cur)
+        elif terms[0] < cur:
+            count = 0
+            while terms:
+                terms.pop()
+                count += 1
+            answer.append(count)
+            terms.append(cur)
+
+        if i == n-1:
+            count = 0
+            while terms:
+                terms.pop()
+                count += 1
+            answer.append(count)
+
+    return answer
+
+
+print(solution([93, 30, 55], [1, 30, 5]))
+
+
+'''
+Brute force 
+O(n)
+'''
+
+'''
+def solution(progresses, speeds):
+
+    n = len(progresses)
+    answer = []
+    terms = []
+    for i in range(n):
+        if(terms and )
+        terms[i] = math.ceil((100 - progresses[i]) / speeds[i])
 
     i = 0
     while i < n:
         count = 1
-        while i + count < n and term[i] >= term[i + count]:
+        while i + count < n and terms[i] >= terms[i + count]:
             count += 1
         i += count
         answer.append(count)
@@ -21,3 +64,4 @@ def solution(progresses, speeds):
 
 
 print(solution(	[93, 30, 55], [1, 30, 5]))
+'''
